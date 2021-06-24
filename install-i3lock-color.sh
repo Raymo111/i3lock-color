33364700a2f5017898eb10553da57cb5cc74b7f6
JezerM <amyuki4@icloud.com>
2021-06-23 21:48:04 -0600

    Tab completion

--- install-i3lock-color.sh
+++ install-i3lock-color.sh
@@ -6,3 +6,11 @@ cd build
 sudo make install
 
 sudo install -Dm644 ../LICENSE "/usr/share/licenses/i3lock-color/LICENSE"
+
+if [[ -f /usr/bin/zsh ]]; then
+  sudo cp ../_i3lock /usr/share/zsh/vendor-completions
+fi
+
+if [[ -f /usr/bin/bash ]]; then
+  sudo cp ../i3lock-bash /usr/share/bash-completion/completions/i3lock
+fi
