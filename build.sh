b7ac46682635199aeb57a71288c47df83bde60d7
Alexandru Nechita <alexandru.italia32@gmail.com>
2023-08-01 19:07:35 +0200

    build: :boom: WIP Migration to meson build system
    WIP Migration of the build system from autotools to meson,
    following the original i3lock new build system.
    Original commits from i3lock:
    https://github.com/i3/i3lock/tree/2.14
    
    Signed-off-by: Alexandru Nechita <alexandru.italia32@gmail.com>

--- build.sh
+++ build.sh
@@ -1,27 +1,9 @@
-#!/bin/sh -x
+#!/bin/sh
 
-configureOpts=""
+rm -rf build/
+mkdir -p build && cd build/
 
-while getopts ":hd" opt; do
-  case ${opt} in
-    h ) echo "Use -d to turn on sanitizers (for debugging only)"
-      exit;;
-    d ) configureOpts="--enable-debug"
-      ;;
-    \? ) echo "Usage: $0 [-h] [-d]"
-      exit;;
-  esac
-done
+meson .. -Dprefix=/usr
+ninja
 
-configureOpts="${configureOpts} --prefix=/usr --sysconfdir=/etc"
-
-autoreconf -fiv
-
-BUILD_DIR=build/
-rm -rf $BUILD_DIR
-mkdir -vp $BUILD_DIR
-cd $BUILD_DIR || { echo "cd $BUILD_DIR"; exit 127; }
-
-../configure ${configureOpts}
-
-make
+# Once builded the executable can be found in the build directory
